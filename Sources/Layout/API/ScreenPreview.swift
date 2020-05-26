175c4da890fe7f042d2b6c0c0af250f63b1bdec4
Vutlhari <github@intelleq.co.za>
2020-05-26 09:27:19 +0200

    Improve flexibility of `previewAsScreen` by specifying device type
    This will not allow the calling site to easily specify what devices
    they want the preview to be showed in. The default preview device
    is the iPhone11

--- Sources/Layout/API/ScreenPreview.swift
+++ Sources/Layout/API/ScreenPreview.swift
@@ -8,10 +8,11 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct ScreenPreview<Screen: View>: View {
+    var devices: [DeviceType]
     var screen: Screen
     
     public var body: some View {
-        ForEach(values: deviceNames) { device in
+        ForEach(values: devices.compactMap { $0.rawValue }) { device in
             ForEach(values: ColorScheme.allCases) { scheme in
                 NavigationView {
                     self.screen
@@ -26,20 +27,11 @@ public struct ScreenPreview<Screen: View>: View {
         }
     }
     
-    private var deviceNames: [String] {
-        [
-            "iPhone 8",
-            "iPhone 11",
-            "iPhone 11 Pro Max",
-            "iPad (7th generation)",
-            "iPad Pro (12.9-inch) (4th generation)"
-        ]
-    }
 }
 
 public extension UIViewController {
     @available(iOS 13.0.0, *)
-    func previewAsScreen() ->  some View {
-        ScreenPreview(screen: self.asPreview())
+    func previewAsScreen(on devices: [DeviceType] = [DeviceType.iPhone11]) ->  some View {
+        ScreenPreview(devices: devices, screen: self.asPreview())
     }
 }
